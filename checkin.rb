ce7761eeb65b12abb7d6ead84690893bf63eb3b3
monsterxcn <monsterxcn@gmail.com>
2020-07-03 05:55:28 +0000

    fix: local ruby test pass

--- checkin.rb
+++ checkin.rb
@@ -4,11 +4,11 @@
 require 'watir'
 require 'webdrivers/chromedriver'
 
-USERNAME = ENV['SECRET_RBID']
-PASSWORD = ENV['SECRET_RBPASS']
+USERNAME = ENV['SECRET_ID']
+PASSWORD = ENV['SECRET_PASS']
 
 puts '========================='
-Watir.default_timeout = 180
+Watir.default_timeout = 120
 args = ['--headless', '--no-sandbox', '--ignore-certificate-errors', '--disable-dev-shm-usage', '--disable-translate', '--window-size=1200x600']
 browser = Watir::Browser.new :chrome, options: {args: args}
 browser.goto 'http://ehome.hrbeu.edu.cn/'
@@ -29,7 +29,7 @@ login(browser, USERNAME, PASSWORD)
 puts '[debug] Login one success...'
 
 # Wait for loading
-browser.wait_until do |b|
+browser.wait_until(timeout: 120, interval: 3) do |b|
   b.div(id: 'div_loader').style.include?('display: none;')
 end
 puts '[debug] Form loaded...'
@@ -49,7 +49,7 @@ browser.button(text: '确定').click
 
 # Check success
 browser.refresh
-browser.wait_until do |b|
+browser.wait_until(timeout: 120, interval: 3) do |b|
   b.div(id: 'div_loader').style.include?('display: none;')
 end
 puts '[debug] ' + browser.div(id: 'title_content').text
