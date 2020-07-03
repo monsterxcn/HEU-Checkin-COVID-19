0510a62e64c6a5fcb89bb9a8e0f0c3722ed8ef5e
monsterxcn <monsterxcn@gmail.com>
2020-07-03 02:50:09 +0000

    Update ruby test

---
+++ checkin.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'watir'
+require 'webdrivers/chromedriver'
+
+USERNAME = ENV['SECRET_RBID'].split('"').last
+PASSWORD = ENV['SECRET_RBPASS'].split('"').last
+
+puts '========================='
+args = ['--headless', '--no-sandbox', '--ignore-certificate-errors', '--disable-dev-shm-usage', '--disable-translate', '--window-size=1920x1080']
+browser = Watir::Browser.new :chrome, options: {args: args}
+Watir.logger.level = :info
+browser.goto 'http://ehome.hrbeu.edu.cn/'
+
+def login(browser, username, password)
+  browser.text_field(id: 'username').set username
+  browser.text_field(id: 'password').set password
+  browser.button(name: 'submit').click
+end
+
+# First login
+login(browser, USERNAME, PASSWORD)
+puts '[debug] Login ehome success...'
+
+# Second login
+browser.goto 'https://one.wvpn.hrbeu.edu.cn/infoplus/form/JKXXSB/start'
+login(browser, USERNAME, PASSWORD)
+puts '[debug] Login one success...'
+
+# Wait for loading
+browser.wait_until do |b|
+  b.div(id: 'div_loader').style.include?('display: none;')
+end
+puts '[debug] Form loaded...'
+
+# Select the checkbox
+cb = browser.checkbox(id: 'V1_CTRL82')
+cb.set unless cb.set?
+puts '[debug] Checkbox checked...'
+
+# Submit
+browser.link(text: '确认填报').click
+sleep 5
+browser.button(text: '好').click
+puts '[debug] Form Submitted...'
+sleep 5
+browser.button(text: '确定').click
+
+# Check success
+browser.refresh
+browser.wait_until do |b|
+  b.div(id: 'div_loader').style.include?('display: none;')
+end
+puts '[debug] ' + browser.div(id: 'title_content').text
+if browser.div(id: 'title_content').text.include?('已完成')
+  puts '[info] Checkin Success!'
+else
+  puts '[error] Checkin Fail!'
+end
+puts '[info] End at ' + Time.now.to_s + '.'
+browser.close
+puts '========================='
