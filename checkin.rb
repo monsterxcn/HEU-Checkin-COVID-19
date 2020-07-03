49683de5f00752ee2d283d1c776e9cb243f8dddf
monsterxcn <monsterxcn@gmail.com>
2020-07-03 06:57:17 +0000

    Update ruby ​​related documents

--- checkin.rb
+++ checkin.rb
@@ -8,7 +8,7 @@ USERNAME = ENV['SECRET_ID']
 PASSWORD = ENV['SECRET_PASS']
 
 puts '========================='
-Watir.default_timeout = 120
+Watir.default_timeout = 300
 args = ['--headless', '--no-sandbox', '--ignore-certificate-errors', '--disable-dev-shm-usage', '--disable-translate', '--window-size=1200x600']
 browser = Watir::Browser.new :chrome, options: {args: args}
 browser.goto 'http://ehome.hrbeu.edu.cn/'
@@ -29,7 +29,7 @@ login(browser, USERNAME, PASSWORD)
 puts '[debug] Login one success...'
 
 # Wait for loading
-browser.wait_until(timeout: 120, interval: 3) do |b|
+browser.wait_until(timeout: 300, interval: 3) do |b|
   b.div(id: 'div_loader').style.include?('display: none;')
 end
 puts '[debug] Form loaded...'
@@ -49,7 +49,7 @@ browser.button(text: '确定').click
 
 # Check success
 browser.refresh
-browser.wait_until(timeout: 120, interval: 3) do |b|
+browser.wait_until(timeout: 300, interval: 3) do |b|
   b.div(id: 'div_loader').style.include?('display: none;')
 end
 puts '[debug] ' + browser.div(id: 'title_content').text
